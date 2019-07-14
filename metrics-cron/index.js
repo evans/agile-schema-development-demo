db730d5f6b40e1e472c75de3421c56daedbbdb8e
Evans Hauser <evanshauser@gmail.com>
2019-07-14 14:42:11 -0700

    add port bind to metrics cron to keep heroku up

--- metrics-cron/index.js
+++ metrics-cron/index.js
@@ -6,5 +6,13 @@ cron.schedule("* * * * *", () => {
   run("android-workload.yml", {});
   run("ios-workload.yml", {});
 });
-// const port = process.env.PORT || "4000";
+const port = process.env.PORT || "4000";
 // run("ios-workload.yml", { variables: JSON.stringify({ port }) });
+
+//keep heroku happy by binding to port
+const http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(port);
