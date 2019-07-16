c3a56f9c3908710cfd0250a33877e6be92108b47
Evans Hauser <evanshauser@gmail.com>
2019-07-16 07:50:27 -0700

    add mutation response

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -23,7 +23,14 @@ module.exports = {
     bookTrips: async (_, { launchIds }, { dataSources }) => {
       const results = await dataSources.userAPI.bookTrips({ launchIds });
 
-      return Boolean(results) && results.length === launchIds.length;
+      return {
+        success: Boolean(results) && results.length === launchIds.length,
+        message:
+          Boolean(results) && results.length === launchIds.length
+            ? "success"
+            : "failed",
+        launchIds
+      };
     },
     cancelTrip: async (_, { launchId }, { dataSources }) => {
       const result = dataSources.userAPI.cancelTrip({ launchId });
