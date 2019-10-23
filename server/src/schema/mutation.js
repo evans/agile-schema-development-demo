49d7a411488261c848e457f5d2ebccdc626127cc
Evans Hauser <evanshauser@gmail.com>
2019-10-23 15:33:19 -0700

    deprecate field and create new with response

--- server/src/schema/mutation.js
+++ server/src/schema/mutation.js
@@ -9,7 +9,14 @@ const typeDefs = gql`
     Book launches as trips for currently logged in user
     if false, booking failed
     """
-    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    bookTrips(launchIds: [ID]!): Boolean!
+      @deprecated(reason: "use bookTripsWithReponse")
+
+    """
+    Book launches as trips for currently logged in user
+    if false, booking failed
+    """
+    bookTripsWithResponse(launchIds: [ID]!): TripUpdateResponse!
 
     """
     Cancel launch for currently logged in user
