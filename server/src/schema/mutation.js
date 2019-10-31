8a58802f9a2154f2c3ef615f9ebafc1e68d93436
Evans Hauser <evanshauser@gmail.com>
2019-10-31 10:13:13 -0700

    deprecated bookTrips and add new field

--- server/src/schema/mutation.js
+++ server/src/schema/mutation.js
@@ -9,7 +9,16 @@ const typeDefs = gql`
     Book launches as trips for currently logged in user
     if false, booking failed
     """
-    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    bookTrips(launchIds: [ID]!): Boolean!
+      @deprecated(
+        reason: "use bookTripsWithResponse, because the data returned can update the cache"
+      )
+
+    """
+    Book launches as trips for currently logged in user
+    if success is false, booking failed
+    """
+    bookTripsWithResponse(launchIds: [ID]!): TripUpdateResponse!
 
     """
     Cancel launch for currently logged in user
